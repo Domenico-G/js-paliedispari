6a1b9dee3ead39dcefa01655bc22c4510de4e1e7
Domenico <domenicograsso@gmail.com>
2021-01-07 15:57:23 +0100

    fine esercizio

--- js-pari-dispari/app.js
+++ js-pari-dispari/app.js
@@ -1,5 +1,5 @@
 // chiedo all'utente un numero da 1 a 5, e di scegliere pario o dispari
-// var evenOdd = prompt('Scegli fra pari o dispari');
+var evenOddUser = prompt('Scegli fra pari o dispari');
 var userNumb = parseInt(prompt('Inserisci un numero da 1 a 5'));
 
 // creo una funzione per generare un numero
@@ -9,3 +9,19 @@ function random(min, max){
 
 // sommo i due numeri
 var somma = userNumb + random(1,5);
+
+// creo una funzione per stabilire se il numero è pari o dispari
+function evenOdd(somma){
+  if (somma % 2 === 0) {
+    return 'pari';
+  } else {
+    return 'dispari';
+  }
+}
+
+// confronto i risultati e indico chi ha vinto
+if (evenOddUser == evenOdd(somma)) {
+  alert('hai vinto')
+} else {
+  alert('Hai perso')
+}
